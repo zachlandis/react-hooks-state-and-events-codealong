f9fa766825a5428a410d48412176a50ca67d7b89
Zach <zachlandis91@gmail.com>
2023-02-14 14:13:06 -0700

    commit tree d476784b3502d44b06f909a029c544d7d2980f5f

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Toggle from "./Toggle";
 
+
 function App() {
   return (
     <div>
       <h3>Toggle</h3>
-      <Toggle />
+      <Toggle/>
     </div>
   );
 }
